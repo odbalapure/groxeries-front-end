e5a22dae081bd91d9fff8f574e8a4a9c6d8e33d4
odbalapure <83666636+odbalapure@users.noreply.github.com>
2021-12-08 22:27:44 +0530

    Add files via upload

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+function Loading() {
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ margin: "45vh auto" }}
+    >
+      <p className="fs-1 fw-bold">Loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
